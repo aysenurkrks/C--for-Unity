f0704acb3097b7214f2d74e6db60e3dd97cfdc53
aysenurkrks <krksaysnr@gmail.com>
2021-12-22 20:58:48 +0300

    Ders1

---
+++ pratikler/consoleprogramlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace consoleprogramlama {
+class Program {
+
+    public static void Main (String [] args){
+
+        Console.WriteLine("Helloooo, World!");
+        Console.WriteLine("isim?");
+        string name = Console.ReadLine();
+        Console.WriteLine("soyisim");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine(name +" "+ surname);
+
+    }
+}
+
+}
