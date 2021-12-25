a8f9bf9f05382083922d17ed3dbc7b0a8c9c4d3a
aysenurkrks <krksaysnr@gmail.com>
2021-12-25 13:16:50 +0300

    arrays

---
+++ pratikler/diziler/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace diziler{
+class Program {
+
+    public static void Main (String [] args){
+
+        string [] renkler = new string[5];
+        string [] hayvanlar = {"aslan", "kedi", "karga", "kuş"};
+        int [] dizi;
+        dizi = new int[5];
+        renkler[0]="mavi";
+        dizi[3]=10;
+
+        Console.WriteLine(hayvanlar[1]);
+        Console.WriteLine(dizi[3]);
+        Console.WriteLine(renkler[0]);
+       
+
+        //Döngüler ile dizi
+
+        Console.WriteLine("Dizinin boyutu ne olsun?");
+        int [] sayilar = new int[int.Parse(Console.ReadLine())];
+
+        for(int i =0; i<sayilar.Length;i++){
+            Console.WriteLine((i+1)+". sayıyı giriniz");
+            sayilar[i]=int.Parse(Console.ReadLine());
+
+        }
+        int toplam=0;
+        foreach (var sayi in sayilar)
+        {
+            toplam+=sayi;
+        }
+        Console.WriteLine(toplam/sayilar.Length);
+
+
+
+
+
+    }
+}
+
+}
